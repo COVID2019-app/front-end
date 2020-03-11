fc3ec27edeb691e413dcfeb0ec5458cbb7e930a7
Min-Huang <minh.huang96@gmail.com>
2020-03-11 14:04:02 -0700

    made a test chart

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 import Home from "./components/Home"
 import Home2 from "./components/Home2";
-
+import USAChart from "./components/usa/USAChart";
 
 function App() {
   return (
     <div className="App">
       <h1>Covid2019</h1>
-      <Home2/>
+      <Home2 />
+      <USAChart />
     </div>
   );
 }
