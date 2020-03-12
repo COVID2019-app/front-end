134ff5f618e5c0bb54c90d43994f8cd14d012759
Min-Huang <minh.huang96@gmail.com>
2020-03-12 13:06:29 -0700

    finished home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './App.css';
+import styled from "styled-components";
 
-import Home from "./components/Home"
-import Home2 from "./components/Home2";
+import Home from "./components/Home";
 import USAChart from "./components/usa/USAChart";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Covid2019</h1>
-      <Home2 />
+    <AppContainer>
+      <h1 style={{textAlign:"center"}}>Covid2019</h1>
+      <Home />
       <USAChart />
-    </div>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
