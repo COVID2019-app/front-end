50f643825012df0b0b01623fcffb616e556f4b44
Shernice Johnson <shernice.johnson@gmail.com>
2020-03-13 19:50:30 -0400

    added server data to USA chart

--- src/components/usa/USAChart.js
+++ src/components/usa/USAChart.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect } from "react";
+import { connect } from 'react-redux';
 
 import Chart, {
   ArgumentAxis,
@@ -7,14 +8,34 @@ import Chart, {
   Series
 } from "devextreme-react/chart";
 
-import { USAConfirmedData } from "./region_confirm_data";
+import { getUsRegions } from '../../store/actions/index';
 
-const USAChart=()=> {
+const USAChart=(props)=> {
+
+
+
+  const { getUsRegions, region_name/*, isFetching*/ } = props;
+  useEffect(() => {
+    getUsRegions();
+
+
+  }, [getUsRegions])
+
+  const data = []
+
+  var x
+
+  for(x in region_name){
+    data.push({
+      arg: region_name[x].iso_code,
+      val: region_name[x].confirmed})
+  }
+
   
     return (
       <Chart
         title="USA Confirmed Cases by Region"
-        dataSource={USAConfirmedData}
+        dataSource={data}
         id="chart"
       >
         <ArgumentAxis tickInterval={10}>
@@ -29,4 +50,12 @@ const USAChart=()=> {
   
 }
 
-export default USAChart;
+const mapStateToProps = state => {
+  return {
+    isFetching: state.isFetching,
+    region_name: state.usa_region,
+  };
+};
+export default connect(mapStateToProps, {
+  getUsRegions,
+})(USAChart);
