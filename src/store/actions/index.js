a13dbaffbf3f69cc05b9d219e7b1313a6ace3ec8
Min-Huang <minh.huang96@gmail.com>
2020-03-13 18:04:49 -0700

    added new func

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,11 +4,25 @@ export const FETCHING_COUNTRY_START = "FETCHING_COUNTRY_START";
 export const FETCHING_COUNTRY_SUCCESS = "FETCHING_COUNTRY_SUCCESS";
 export const FETCHING_COUNTRY_FAILURE = "FETCHING_COUNTRY_FAILURE";
 
-export const getCountryList = (info) => dispatch => {
-  console.log(info)
+export const getCountryList_home = info => dispatch => {
+  console.log(info);
+  dispatch({ type: FETCHING_COUNTRY_START });
+  axios
+    .get("https://cvid.herokuapp.com/country")
+    .then(res => {
+      console.log("comments  from server-home :", res);
+
+      dispatch({ type: FETCHING_COUNTRY_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+export const getCountryList = () => dispatch => {
          dispatch({ type: FETCHING_COUNTRY_START });
          axios
-           .get("https://cvid.herokuapp.com/country", info.category)
+           .get("https://cvid.herokuapp.com/country")
            .then(res => {
              console.log("comments  from server :", res);
               
