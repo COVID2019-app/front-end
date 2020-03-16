51074578ff685fc8f55141b381cb04b7af7e9288
Mike Harley <mharley12345@gmail.com>
2020-03-16 00:38:21 -0700

    adds scss replaces css files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { createStore, applyMiddleware /*compose*/ } from "redux";
