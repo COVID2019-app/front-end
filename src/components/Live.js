5ad7de17e18ec7f6b6007fd99a612036a3526118
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2020-03-16 09:27:09 +0100

    Use a top nav bar and remove unused links for now
    We also make some mobile/tablet changes.

--- src/components/Live.js
+++ src/components/Live.js
@@ -1,57 +1,53 @@
 import React, { useEffect } from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import { Card, CardText, CardBody, CardGroup, CardHeader } from 'reactstrap';
-import { getCountryList } from "../store/actions";
-import {withRouter} from 'react-router-dom';
+import { getCountryList } from '../store/actions';
+import { withRouter } from 'react-router-dom';
 
+function Live(props) {
+  const { getCountryList, country } = props;
 
+  useEffect(() => {
+    getCountryList();
+  }, [getCountryList]);
 
-
-function Live (props) {
-
-    const { getCountryList, country} = props;
-    
-    useEffect(() => {
-        getCountryList();
-    }, [getCountryList]);
-
-    
+  return (
+    <CardGroup>
+      {country.slice(0, 24).map(x => {
         return (
-            <div className="container">
-                <div className="col-lg-10 offset-lg-2">
-                    <CardGroup>
-
-                        {country.slice(0,24).map((x) => {
-
-                            return (
-                                <div key={x.country_id} className="col-sm-3 live-container ">
-                                    <Card >
-                                        <CardHeader>{x.country_name === "United States of America" ? "USA" : x.country_name}</CardHeader>
-                                        <CardBody className="live-card">
-                                            <CardText className="live-cases">{x.confirmed_cases.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</CardText>
-                                            <CardText className="live-deaths">{x.deaths.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</CardText>
-                                            {/*<Button>Go to Page</Button>*/}
-                                        </CardBody>
-                                    </Card>
-                                </div>
-                            );
-                        })}
-                    </CardGroup>
-                </div>
-            </div>
+          <div key={x.country_id} className="col-sm-3 live-container ">
+            <Card>
+              <CardHeader>
+                {x.country_name === 'United States of America'
+                  ? 'USA'
+                  : x.country_name}
+              </CardHeader>
+              <CardBody className="live-card">
+                <CardText className="live-cases">
+                  {x.confirmed_cases.toLocaleString(navigator.language, {
+                    minimumFractionDigits: 0,
+                  })}
+                </CardText>
+                <CardText className="live-deaths">
+                  {x.deaths.toLocaleString(navigator.language, {
+                    minimumFractionDigits: 0,
+                  })}
+                </CardText>
+                {/*<Button>Go to Page</Button>*/}
+              </CardBody>
+            </Card>
+          </div>
         );
-    
+      })}
+    </CardGroup>
+  );
 }
 
-
 const mapStateToProps = state => {
-    return {
-        //isFetching: state.isFetching,
-        country: state.country
-    };
+  return {
+    //isFetching: state.isFetching,
+    country: state.country,
+  };
 };
 
-export default withRouter(connect(mapStateToProps, {getCountryList})(Live));
-
-
-
+export default withRouter(connect(mapStateToProps, { getCountryList })(Live));
