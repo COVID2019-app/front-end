9059977e8f5b5e51505085151ec98b21dc17f274
Mike Harley <mharley12345@gmail.com>
2020-03-15 22:26:14 -0700

    .

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -5,7 +5,7 @@ import {COUNTRIES} from '../shared/livecountries';
 import { Button } from 'devextreme-react/button';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.dark.css';
-import logo from '../shared/images/LOGO.svg'
+import LOGO from '../shared/images/LOGO.svg'
 //import UsaRegionsChart from '../components/usa/UsaRegionsChart'
 
 
@@ -51,7 +51,7 @@ const SideMenu = (props) => {
           
                 <div className="col-lg-2 sidebar sidebar-sicky bg-light">
                   <div className="container">
-                  <img src={logo} width="125" alt="covid2019 logo"/>
+                  <img src={LOGO} width="125" alt="covid2019 logo"/>
                     <Nav onClick={toggleNavbar} vertical>
                       Dashboard
                       <NavItem>
