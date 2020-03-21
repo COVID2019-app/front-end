d95eff252041822f8485070145a034e086bb1fd6
Mike Harley <mharley12345@gmail.com>
2020-03-21 15:59:22 -0700

    Update baseUrl.js

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,3 +1,3 @@
 //export const baseUrl = 'https://staging-cvid.herokuapp.com/';
 
-export const baseUrl = 'https://staging-cvid.herokuapp.com/';
+export const baseUrl = 'https://cvid.herokuapp.com/';
