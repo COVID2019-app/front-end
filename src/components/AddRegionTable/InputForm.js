f9ffb1e47b6b3d228daa325dba3eb175c19663a7
Min-Huang <minh.huang96@gmail.com>
2020-03-23 01:18:17 -0700

    removed alter

--- src/components/AddRegionTable/InputForm.js
+++ src/components/AddRegionTable/InputForm.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Form, FormGroup, Label, Input, Alert, Button } from 'reactstrap';
+import React, { useState } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 function InputForm(prop) {
   // form input setup
