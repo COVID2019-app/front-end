f56e483291442c10eaccdf33f7f00cbf219a7b3f
Min-Huang <minh.huang96@gmail.com>
2020-03-23 22:38:20 -0700

    added post daily region data action, updated regions for some countries on region_add.js

--- src/shared/country_add.js
+++ src/shared/country_add.js
@@ -11,4 +11,28 @@ export const COUNTRIES = [
     name: 'Italy',
     country_id: 25,
   },
+  {
+    name: 'Iran',
+    country_id: 32,
+  },
+  {
+    name: 'Korea, Republic of',
+    country_id: 6,
+  },
+  {
+    name: 'Spain',
+    country_id: 29,
+  },
+  {
+    name: 'Germany',
+    country_id: 19,
+  },
+  {
+    name: 'Switzerland',
+    country_id: 42,
+  },
+  {
+    name: 'Iraq',
+    country_id: 38,
+  },
 ];
